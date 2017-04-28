a4b38e9306ab02c3a0a58300d2966b979c58862c
Chris van Run <C.P.A.vanrun@uu.nl>
2017-04-28 11:42:45 +0200

    Upgrade 1.12 req to 1.14 req

--- linux-terminal.sh
+++ linux-terminal.sh
@@ -3,7 +3,7 @@
 #-------------------------------------------------------------------------------
 
 # Define required Zep version:
-ZEP_VER=1.12
+ZEP_VER=1.14
 
 # Iff installed at nonstandard location, define installation location here:
 #ZEP_DIR=/opt/zep
--- windows-terminal.bat
+++ windows-terminal.bat
@@ -3,7 +3,7 @@
 rem ----------------------------------------------------------------------------
 
 rem Define required Zep version:
-set ZEP_VER=1.12
+set ZEP_VER=1.14
 
 rem Iff installed at nonstandard location, define installation location here:
 rem set ZEP_DIR=C:\zep
